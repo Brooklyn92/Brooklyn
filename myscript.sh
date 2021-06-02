53f2971b838f382a196dfae89f76b1c9223f22ab
Brooklyn <yurchenko221292@yandex.ru>
2021-06-02 19:46:20 +0400

    Commit script Bush

---
+++ myscript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd Brooklyn
+mkdir BMW Ford Caddilak
+cd Ford
+touch  focus.txt fiesta.txt fison.txt Mustang.json Mondeo.json
+mkdir Focus Mondeo Mustang
+ls
+mv focus.txt fiesta.txt Focus
