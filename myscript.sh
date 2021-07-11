792bfe3f2471b54ff6fe486006253371e38a03f1
Brooklyn <yurchenko221292@yandex.ru>
2021-07-11 21:33:05 +0400

    deleted files

--- myscript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd Brooklyn
-mkdir BMW Ford Caddilak
-cd Ford
-touch  focus.txt fiesta.txt fison.txt Mustang.json Mondeo.json
-mkdir Focus Mondeo Mustang
-ls
-mv focus.txt fiesta.txt Focus
